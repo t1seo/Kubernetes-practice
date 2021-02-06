a80a0c7e181da006d115bad48e7300fb866a1e9f
Seo Tae-won <t1won.seo@gmail.com>
2021-02-06 22:44:03 +0900

    kubernetes tutorial

--- hello.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var content = function(req, resp) {
- resp.end("Hello Kubernetes!" + "\n");
- resp.writeHead(200);
-}
-var w = http.createServer(content);
-w.listen(8000);
